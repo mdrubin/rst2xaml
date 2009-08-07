1d3014332e949d7625f29b89ef72689815c93e6b
fuzzyman <fuzzyman@d71e87e8-83a4-11de-816b-7b977620c210>
2009-08-07 22:55:00 +0000

    Initial commit

---
+++ rst2xaml.py
@@ -0,0 +1,27 @@
+import sys
+
+from docutils.core import publish_string
+from xamlwriter.translator import XamlTranslator
+from xamlwriter.writer import XamlWriter
+
+USAGE = "rst2xaml input_file output_file"
+
+if len(sys.argv) != 3:
+    print USAGE
+
+    
+input_data = open(sys.argv[1]).read()
+
+
+settings_overrides = {
+    'file_insertion_enabled': False,
+}
+rv = publish_string(source=input_data, writer=XamlWriter(),
+                    settings_overrides=settings_overrides)
+
+output = rv.root.to_string()
+print output
+
+#handle = open(sys.argv[2])
+#handle.write(rv.root.to_string())
+#handle.close()
---
+++ xamlwriter/tests/testxamlwriter.py
@@ -0,0 +1,15 @@
+import unittest
+
+from xamlwriter import XamlTranslator
+from xamlwriter.node import Node, TextNode
+
+
+class TestXamlWriter(unittest.TestCase):
+    
+    def testBasic(self):
+        self.fail()
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
