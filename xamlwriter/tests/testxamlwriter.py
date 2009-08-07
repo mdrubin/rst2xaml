383c04de7d18091df50aa7646ed4af15aa34b954
fuzzyman <fuzzyman@d71e87e8-83a4-11de-816b-7b977620c210>
2009-08-07 23:56:33 +0000

    Starting tests for the XamlWriter

--- xamlwriter/tests/testxamlwriter.py
+++ xamlwriter/tests/testxamlwriter.py
@@ -1,13 +1,27 @@
 import unittest
 
-from xamlwriter.translator import XamlTranslator
+from xamlwriter.writer import XamlWriter
 from xamlwriter.node import Node, TextNode
+
+from docutils.core import publish_string
+
+
+settings_overrides = {
+    'file_insertion_enabled': False,
+}
+
+def tree_from_string(input_data):
+    rv = publish_string(source=input_data, writer=XamlWriter(),
+                        settings_overrides=settings_overrides)
+
+    return rv.root
 
 
 class TestXamlWriter(unittest.TestCase):
     
     def testBasic(self):
-        self.fail()
+        tree = tree_from_string('')
+        self.assertEqual(tree, Node('Document'))
 
 
 if __name__ == '__main__':
