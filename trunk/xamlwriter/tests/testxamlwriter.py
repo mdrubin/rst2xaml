f58991e88a41d0d023f6af10d185f07b48a98c97
fuzzyman <fuzzyman@d71e87e8-83a4-11de-816b-7b977620c210>
2009-08-08 11:33:35 +0000

    Tests for the functionality that works so far

--- trunk/xamlwriter/tests/testxamlwriter.py
+++ trunk/xamlwriter/tests/testxamlwriter.py
@@ -22,6 +22,37 @@ class TestXamlWriter(unittest.TestCase):
     def testBasic(self):
         tree = tree_from_string('')
         self.assertEqual(tree, Node('Document'))
+    
+        
+    def testParagraph(self):
+        tree = tree_from_string('Hello')
+        
+        node = Node('Document')
+        node.children.append(Node('Paragraph'))
+        node.children[0].children.append(TextNode('Hello'))
+        self.assertEqual(tree.to_string(), node.to_string())
+    
+        
+    def testItalics(self):
+        tree = tree_from_string('*Hello*')
+        
+        node = Node('Document')
+        para = Node('Paragraph')
+        para.children.append(Node('Italic'))
+        para.children[0].children.append(TextNode('Hello'))
+        node.children.append(para)
+        self.assertEqual(tree.to_string(), node.to_string())
+    
+        
+    def testBold(self):
+        tree = tree_from_string('**Hello**')
+        
+        node = Node('Document')
+        para = Node('Paragraph')
+        para.children.append(Node('Bold'))
+        para.children[0].children.append(TextNode('Hello'))
+        node.children.append(para)
+        self.assertEqual(tree.to_string(), node.to_string())
 
 
 if __name__ == '__main__':
