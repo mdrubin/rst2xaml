8679a8a04ac4e58c7650fda73e7b12fed7de71ae
fuzzyman <fuzzyman@d71e87e8-83a4-11de-816b-7b977620c210>
2009-08-10 23:23:34 +0000

    Adding test for the pygments formatter.

---
+++ xamlwriter/tests/testpygments.py
@@ -0,0 +1,53 @@
+import unittest
+
+from textwrap import dedent
+
+from xamlwriter.writer import publish_xaml
+
+# This installs the pygments directive
+import xamlwriter.register_directive
+
+
+def make_source(string):
+    return '.. sourcecode:: python\n\n    ' +  '\n    '.join(string.splitlines())
+
+def make_doc(string):
+    return ('<FlowDocument xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" '
+            'xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml">'
+            '<Paragraph FontFamily="monospace" xml:space="preserve">%s'
+            '</Paragraph></FlowDocument>' % string)
+
+
+class TestPygments(unittest.TestCase):
+    
+    def testComment(self):
+        source = make_source('# comment')
+        
+        expected = make_doc('<Run Foreground="#408080" FontStyle="Italic"># comment</Run><Run></Run>\n')
+        self.assertEqual(publish_xaml(source), expected)
+    
+
+    def testXamlEscape(self):
+        self.fail()
+
+
+    def DONTtestClass(self):
+        source = make_source("""\
+        import foo
+        
+        assert something
+        
+        class Foo(object):
+            def method(self, arg1, arg2=None):
+                if a == b:
+                    return 3
+                
+                raise Exception("Weird error")
+        """)
+        
+        print publish_xaml(source)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
