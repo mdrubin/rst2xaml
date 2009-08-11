68692f2fbb5480a4902d9b93b607c94f92938456
fuzzyman <fuzzyman@d71e87e8-83a4-11de-816b-7b977620c210>
2009-08-11 19:14:19 +0000

    Switch to UTF8 for scripts.

--- rst2xamlsl.py
+++ rst2xamlsl.py
@@ -15,12 +15,12 @@ if len(sys.argv) != 3:
     print USAGE
     sys.exit(1)
     
-input_data = open(sys.argv[1]).read()
+input_data = open(sys.argv[1]).read().decode('utf-8')
 #print input_data
 
 output = publish_xaml(input_data, flowdocument=False)
 #print output
 
 handle = open(sys.argv[2], 'w')
-handle.write(output)
+handle.write(output.encode('utf-8'))
 handle.close()
