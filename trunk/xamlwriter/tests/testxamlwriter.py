5b6d70f33dd34a58dc422efe476ca4386bb104df
fuzzyman <fuzzyman@d71e87e8-83a4-11de-816b-7b977620c210>
2009-08-18 23:21:00 +0000

    Still tinkering with margins.

--- trunk/xamlwriter/tests/testxamlwriter.py
+++ trunk/xamlwriter/tests/testxamlwriter.py
@@ -378,29 +378,6 @@ class TestSilverlightXaml(unittest.TestCase):
         self.assertEqual(actual, node)
     
     
-    def testBulletList(self):
-        node = get_root_sl()
-        def get_item(text):
-            node = Node('ListItem')
-            para = Node('Paragraph')
-            para.children.append(TextNode(text))
-            node.children.append(para)
-            return node
-        
-        list_node = Node('List')
-        list_node.children.append(get_item('first'))
-        list_node.children.append(get_item('second'))
-        list_node.children.append(get_item('third'))
-        
-        node.children.append(list_node)
-        
-        actual = tree_from_string_sl("""\
-            * first
-            * second
-            * third""")
-        
-        self.assertEqual(actual, node)
-
 
 if __name__ == '__main__':
     unittest.main()
